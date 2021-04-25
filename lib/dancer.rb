cfa6e09dfda21c44d6c147f64c8186a40f761fb4
Cody Alvarez <cody_alvarez19@outlook.com>
2021-04-25 02:24:38 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,4 @@
 class Dancer
-  extend MetaDancing
-  include Dance
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
