bd75dac29bd6f73dba2194575afb218f722bdea8
Cody Alvarez <cody_alvarez19@outlook.com>
2021-04-25 02:14:22 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,10 +1,10 @@
 module Dance
 
-  def twirl 
+  def twirl
     "I'm twirling!"
   end
 
-  def jump 
+  def jump
     "Look how high I'm jumping!"
   end
 
@@ -12,7 +12,7 @@ module Dance
     "I'm doing a pirouette"
   end
 
-  def take_a_bow 
+  def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
 end
